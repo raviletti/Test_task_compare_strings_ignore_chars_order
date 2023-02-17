5291c2583e36eaec352d37a521d2fd4466ceea16
raviletti <raviletti@gmail.com>
2023-02-18 02:42:21 +0400

    fix the compare method

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -4,31 +4,33 @@ import java.util.stream.Collectors;
 
 public class Solution {
     public static void main(String[] args) {
-        String[] array = {"a", "qwe", "ewq","wqe","qwee","a","a", "af", "ad", "da", "iamlordvoldemort", "tommarvoloriddle"};
+        String[] array = {"a", "qwe", "ewq","wqe","qwee","a","a", "af", "ad", "da", "iamlordvoldemort", "tommarvoloriddle", "qq", "qq", "pr", "rp", "a"};
         compareIgnoreOrder(array);
     }
 
 
     public static String compareIgnoreOrder(String[] strings){
-        List<Integer> stringCharsAsInts = Arrays.stream(strings).map(e -> {
-            int charsCounter = 0;
-            for (int k = 0; k < e.length(); k++) {
-                charsCounter += e.charAt(k);
+        List<String> stringCharsAsInts = Arrays.stream(strings).map(e -> {
+            char[] chars = e.toCharArray();
+            Arrays.sort(chars);
+            StringBuilder str = new StringBuilder();
+            for(char ch : chars){
+                str.append(ch);
             }
-            return charsCounter;
+            return str.toString();
         }).toList();
 
-        Map<Integer, Long> pairMatches = stringCharsAsInts.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        List<Integer> stringCharsAsIntsCopy = new ArrayList<>(stringCharsAsInts);
+        Map<String, Long> pairMatches = stringCharsAsInts.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        List<String> stringCharsAsIntsCopy = new ArrayList<>(stringCharsAsInts);
         StringBuilder result = new StringBuilder();
 
-        for(Integer val : pairMatches.keySet()){
+        for(String val : pairMatches.keySet()){
             if(pairMatches.get(val) > 1){
                 result.append(strings[stringCharsAsInts.indexOf(val)]).append(" = ");
                 for(int i=0; i<pairMatches.get(val); i++){
                     int curentIndex =  stringCharsAsIntsCopy.indexOf(val);
                     result.append(curentIndex).append(i==pairMatches.get(val)-1 ? ". " : ", ");
-                    stringCharsAsIntsCopy.set(curentIndex, 0);
+                    stringCharsAsIntsCopy.set(curentIndex, null);
                 }
             }
         }
