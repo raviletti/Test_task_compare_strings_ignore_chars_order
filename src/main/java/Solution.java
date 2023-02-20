0f6220105d9757272f9a7537ef5858a1d03aea3a
raviletti <raviletti@gmail.com>
2023-02-20 10:32:45 +0400

    refactor

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -4,13 +4,13 @@ import java.util.stream.Collectors;
 
 public class Solution {
     public static void main(String[] args) {
-        String[] array = {"a", "qwe", "ewq","wqe","qwee","a","a", "af", "ad", "da", "iamlordvoldemort", "tommarvoloriddle", "qq", "qq", "pr", "rp", "a"};
+        String[] array = {"qqw", "pr", "rp", "qwq", "wqq", "w"};
         compareIgnoreOrder(array);
     }
 
 
     public static String compareIgnoreOrder(String[] strings){
-        List<String> stringCharsAsInts = Arrays.stream(strings).map(e -> {
+        List<String> stringCharsAsList = Arrays.stream(strings).map(e -> {
             char[] chars = e.toCharArray();
             Arrays.sort(chars);
             StringBuilder str = new StringBuilder();
@@ -20,17 +20,17 @@ public class Solution {
             return str.toString();
         }).toList();
 
-        Map<String, Long> pairMatches = stringCharsAsInts.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        List<String> stringCharsAsIntsCopy = new ArrayList<>(stringCharsAsInts);
+        Map<String, Long> pairMatches = stringCharsAsList.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        List<String> stringCharsAsListCopy = new ArrayList<>(stringCharsAsList);
         StringBuilder result = new StringBuilder();
 
         for(String val : pairMatches.keySet()){
             if(pairMatches.get(val) > 1){
-                result.append(strings[stringCharsAsInts.indexOf(val)]).append(" = ");
+                result.append(strings[stringCharsAsList.indexOf(val)]).append(" = ");
                 for(int i=0; i<pairMatches.get(val); i++){
-                    int curentIndex =  stringCharsAsIntsCopy.indexOf(val);
+                    int curentIndex =  stringCharsAsListCopy.indexOf(val);
                     result.append(curentIndex).append(i==pairMatches.get(val)-1 ? ". " : ", ");
-                    stringCharsAsIntsCopy.set(curentIndex, null);
+                    stringCharsAsListCopy.set(curentIndex, null);
                 }
             }
         }
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -6,8 +6,9 @@ public class Test {
     public Object [][] data(){
         return new Object[][]{
                 {"qwe", "ewq","wqe","qwee","a", "qwe = 0, 1, 2. "},
-                {"af", "ad", "da", "iamlordvoldemort", "tommarvoloriddle", "iamlordvoldemort = 3, 4. ad = 1, 2. "},
-                {"2", "54", "45", "121", "2", "2 = 0, 4. 54 = 1, 2. "}
+                {"af", "ad", "da", "iamlordvoldemort", "tommarvoloriddle", "ad = 1, 2. iamlordvoldemort = 3, 4. "},
+                {"2", "54", "45", "121", "2", "54 = 1, 2. 2 = 0, 4. "},
+                {"qqw", "pr", "rp", "qwq", "wqq", "pr = 1, 2. qqw = 0, 3, 4. "}
         };
     }
 
